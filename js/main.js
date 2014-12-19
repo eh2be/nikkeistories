0453c21af0ee09b53a85ccef7b8e35db82fab4cf
Greg Masuda <gregmasuda@gmail.com>
2014-12-18 20:39:29 -0800

    updated style

--- js/main.js
+++ js/main.js
@@ -1,44 +1,9 @@
 $(document).ready(function(){
 
-  $('.responsive').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-
   $('.single-item').slick({
     autoplay: true,
     autoplaySpeed: 5000,
         dots: true,
   });
-
-
 
 });
