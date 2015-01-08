b0baf66e2e2cae65fb77f673be2e23207f032dbd
Greg Masuda <gregmasuda@gmail.com>
2015-01-08 15:27:52 -0800

    working fullpage.js with extras

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    verticalCentered: false
-
+    verticalCentered: false,
+    resize: true,
+    anchors: ['home', 'chronology', 'maps', 'videos', 'education', 'contact', 'credits'],
+    sectionsColor: ['#8FB98B', '#DE564B', '#EAE1C0'],
+    menu: '#menu',
+    slidesNavigation: true,
+    navigation: true,
+    navigationTooltips: ['Home', 'Chronology', 'Maps', 'Videos', 'Education', 'Contact', 'Credits'],
+    continuousVertical: true,
+    // scrollBar: true, //scrollbar make the pages snap more abruptly
+    // responsive: 600
     });
   });
