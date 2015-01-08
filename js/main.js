28335819087ec2ecfde329268ed0d81b4bfe1a09
Greg Masuda <gregmasuda@gmail.com>
2015-01-07 19:25:59 -0800

    last ver of promo site with carousel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 
   $('.single-item').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-        dots: true,
+    dots: true,
+    autoplay: true,       // autoplay
+    autoplaySpeed: 5000,  // autoplay
+    infinite: true,       // fade
+    speed: 1000,          // fade
+    fade: true,           // fade
+    slide: 'div',         // fade
+    cssEase: 'ease'     // fade
   });
 
 });
