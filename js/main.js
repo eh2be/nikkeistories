21a11bb84d3b30b27ffc6275fbbfda71e88aed8a
Greg Masuda <gregmasuda@gmail.com>
2015-01-12 22:38:57 -0800

    chrono inverted scroll abandoned

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     verticalCentered: false,
-    resize: false,
+    resize: true,
     anchors: ['home', 'chronology', 'maps', 'videos', 'education', 'contact', 'credits'],
     sectionsColor: ['#8FB98B', '#DE564B', '#EAE1C0'],
     menu: '#menu',
@@ -9,7 +9,8 @@ $(document).ready(function() {
     navigation: true,
     navigationTooltips: ['Home', 'Chronology', 'Maps', 'Videos', 'Education', 'Contact', 'Credits'],
     continuousVertical: true,
-    // scrollBar: true, //scrollbar make the pages snap more abruptly
+    scrollOverflow: true,
+    scrollBar: false, //scrollbar make the pages snap more abruptly; Foundation off-canvas navbar doesn't work either.
     // responsive: 600
     });
 
