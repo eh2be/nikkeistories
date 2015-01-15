2db34ac5df0fcf8a9e7467b057b43e47de2c1ad8
Greg Masuda <gregmasuda@gmail.com>
2015-01-14 16:30:29 -0800

    no hanges

--- js/main.js
+++ js/main.js
@@ -5,14 +5,19 @@ $(document).ready(function() {
     resize: true,
     anchors: ['home', 'chronology', 'maps', 'videos', 'education', 'contact', 'credits'],
     sectionsColor: ['#8FB98B', '#DE564B', '#EAE1C0'],
-    menu: '#menu',
+    touchSensitivity: 1, //test this
+    // Navigation //
+    // menu: '#menu',
     slidesNavigation: true,
     navigation: true,
     navigationTooltips: ['Home', 'Chronology', 'Maps', 'Videos', 'Education', 'Contact', 'Credits'],
     continuousVertical: true,
+    // Scrolling //
     scrollOverflow: true,
+    autoScrolling: true,
     scrollBar: false, //scrollbar make the pages snap more abruptly; Foundation off-canvas navbar doesn't work either.
     // responsive: 600
+    // normalScrollElements: '#map-canvas, #map-canvas2' // swipe/scroll doesn't work over maps on macbook
     });
 
   $('.video-link').magnificPopup({
